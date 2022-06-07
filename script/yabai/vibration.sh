10ebadde4a88d09dc421e313625b2ac964c7e4d7
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-06-07 22:11:45 +0900

    fix vibration

--- script/yabai/vibration.sh
+++ script/yabai/vibration.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-yabai -m window --move rel:0:0 &
-wait
-sleep 0s &
+yabai -m window --move rel:0:0 
+sleep 0s
 wait
 
-yabai -m window --move rel:4:4 &
-wait
-sleep 300s &
+yabai -m window --move rel:4:4
+sleep 300s
 wait
 
 yabai -m window --move rel:-4:-4
