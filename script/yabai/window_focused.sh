954515e87f4f0ec6a15e410e63ba0498f9acd98e
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-07-02 22:58:06 +0900

    yabai vibration

--- script/yabai/window_focused.sh
+++ script/yabai/window_focused.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-a=("0xFFFFFF00" "0xFFFF00FF" "0xFF00FFFF" "0x00FFFFFF" "0xF0F0F0FF" "0xF0F0FFF0")
+yabai -m window --move rel:2:6
+sleep 0.03
 
-yabai -m config active_window_border_color $(shuf -n1 -e "${a[@]}")
-yabai -m config window_border_width 20
-yabai -m config window_border_width 1
-sleep 10s
+yabai -m window --move rel:-2:-6
+sleep 0.03
 
-yabai -m config active_window_border_color $(shuf -n1 -e "${a[@]}")
-yabai -m config window_border_width 20
-yabai -m config window_border_width 1
-sleep 10s
+yabai -m window --move rel:5:3
+sleep 0.03
 
-yabai -m config active_window_border_color $(shuf -n1 -e "${a[@]}")
-yabai -m config window_border_width 20
-yabai -m config window_border_width 1
+yabai -m window --move rel:-5:-3
