232c4eb0c64e7cc1402d3bb3f0e81e77002d7c29
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-07-06 18:08:19 +0900

    fix vibration

--- script/yabai/window_focused.sh
+++ script/yabai/window_focused.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-yabai -m window --move rel:2:6
+yabai -m window --move rel:1:2
 sleep 0.03
 
-yabai -m window --move rel:-2:-6
+yabai -m window --move rel:-1:-2
 sleep 0.03
 
-yabai -m window --move rel:5:3
+yabai -m window --move rel:2:1
 sleep 0.03
 
-yabai -m window --move rel:-5:-3
+yabai -m window --move rel:-2:-1
