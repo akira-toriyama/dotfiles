634ad912f55ac5abc81190299f4108f5cf570d63
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-07-08 15:17:25 +0900

    focused

--- script/yabai/window_focused.sh
+++ script/yabai/window_focused.sh
@@ -12,7 +12,16 @@ sleep 0.03
 yabai -m window --move rel:-2:-1
 sleep 0.03
 
+#
+
 yabai -m window --move rel:1:2
 sleep 0.03
 
 yabai -m window --move rel:-1:-2
+sleep 0.03
+
+yabai -m window --move rel:2:1
+sleep 0.03
+
+yabai -m window --move rel:-2:-1
+sleep 0.03
