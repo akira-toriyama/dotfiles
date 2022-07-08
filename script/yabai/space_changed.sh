0a99d4fcf6160b569d88700030ce6ef020013039
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-07-08 19:26:50 +0900

    yabai

---
+++ script/yabai/space_changed.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+sleep 0.1
+
+yabai -m window --move rel:2:3
+sleep 0.03
+
+yabai -m window --move rel:-2:-3
+sleep 0.03
+
+yabai -m window --move rel:3:2
+sleep 0.03
+
+yabai -m window --move rel:-3:-2
+sleep 0.03
+
+#
+
+yabai -m window --move rel:2:3
+sleep 0.03
+
+yabai -m window --move rel:-2:-3
+sleep 0.03
+
+yabai -m window --move rel:3:2
+sleep 0.03
+
+yabai -m window --move rel:-3:-2
+sleep 0.03
+
--- script/yabai/window_focused.sh
+++ script/yabai/window_focused.sh
@@ -11,17 +11,3 @@ sleep 0.03
 
 yabai -m window --move rel:-2:-1
 sleep 0.03
-
-#
-
-yabai -m window --move rel:1:2
-sleep 0.03
-
-yabai -m window --move rel:-1:-2
-sleep 0.03
-
-yabai -m window --move rel:2:1
-sleep 0.03
-
-yabai -m window --move rel:-2:-1
-sleep 0.03
