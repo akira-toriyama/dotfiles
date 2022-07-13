328fd9d60821e6bedf8f59a25bc1c23675f42c19
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-07-13 22:25:54 +0900

    fix yabai

--- script/yabai/window_focused.sh
+++ script/yabai/window_focused.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-yabai -m window --move rel:1:2
-sleep 0.03
+sleep 0.03 &
+wait $!
 
-yabai -m window --move rel:-1:-2
-sleep 0.03
+yabai -m window --move rel:10:5 &
+wait $!
+sleep 0.03 &
+wait $!
 
-yabai -m window --move rel:2:1
-sleep 0.03
+yabai -m window --move rel:-10:-5 &
+wait $!
+sleep 0.03 &
+wait $!
 
-yabai -m window --move rel:-2:-1
-sleep 0.03
