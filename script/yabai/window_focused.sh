8dbc9a3d7e058d673777b4c3f453dc4ac7e00576
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-07-19 15:27:18 +0900

    fix todo

--- script/yabai/window_focused.sh
+++ script/yabai/window_focused.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-sleep 0.03 &
-wait $!
+# sleep 0.03 && 
 
-yabai -m window --move rel:10:5 &
-wait $!
-sleep 0.03 &
-wait $!
+# sleep 0.03 &
+# wait $!
 
-yabai -m window --move rel:-10:-5 &
-wait $!
-sleep 0.03 &
-wait $!
+# yabai -m window --move rel:10:5 &
+# wait $!
+# sleep 0.03 &
+# wait $!
 
+# yabai -m window --move rel:-10:-5 &
+# wait $!
+# sleep 0.03 &
+# wait $!
+
+
+sleep 0.03 && yabai -m window --move rel:10:5 && sleep 0.03 && yabai -m window --move rel:-10:-5 &
