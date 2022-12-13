0ccf77b688b2d16581f6623f348ba4a222c16ec2
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-12-13 23:13:18 +0900

    :technologist:[x.x.x]: Fix hoipoiCapsule

--- .githooks/commitHook.ts
+++ .githooks/commitHook.ts
@@ -1,4 +1,4 @@
-import * as hoipoiCapsule from "https://deno.land/x/hoipoi_capsule@v1.2.0/mod.ts";
+import * as hoipoiCapsule from "https://deno.land/x/hoipoi_capsule@v2.0.0/mod.ts";
 
 const o = await hoipoiCapsule.preset.fillInCommitMessage.gitmojiStyle
   .initialize();
