660fb48a3c85158a13eb55fc33f424921cb37aa8
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2024-04-13 13:38:17 +0900

    :hammer:`x.x.x`: Fix karabiner.sh

--- script/karabiner/generation.sh
+++ script/karabiner/generation.sh
@@ -1,3 +1,3 @@
-deno run ~/dotfiles/script/karabiner/karabinerJson.ts > ~/.config/karabiner/karabiner.json && \
-open '/Applications/Karabiner-Elements.app' && \ 
+deno run ~/dotfiles/script/karabiner/karabinerJson.ts > ~/.config/karabiner/karabiner.json
+open '/Applications/Karabiner-Elements.app'
 echo "Devices の マウスを on"
