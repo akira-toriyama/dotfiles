aa58744dbf5d091cf28c4b7b4a74065ff4525c30
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2024-04-23 20:29:04 +0900

    :technologist:`x.x.x`: Fix focused

--- script/yabai/window_focused.sh
+++ script/yabai/window_focused.sh
@@ -5,6 +5,8 @@ yabai -m window --resize bottom:0:-4
 yabai -m window --resize right:-4:0
 yabai -m window --resize left:4:0
 
+sleep 0.05
+
 yabai -m window --resize top:0:-4
 yabai -m window --resize bottom:0:4
 yabai -m window --resize right:4:0
