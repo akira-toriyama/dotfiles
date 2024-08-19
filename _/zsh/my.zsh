ef04ffbdefabddd15d94aaa567dadda3287a19bf
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2024-08-19 23:33:28 +0900

    :sparkles:`x.o.x`: Add sleepwatcher Close #82 (#83)
    * :sparkles:`x.o.x`: Add sleepwatcher Close #82
    
    * :green_heart:`x.x.x`: Fix
    
    * :pencil2:`x.x.o`: Fix

--- _/zsh/my.zsh
+++ _/zsh/my.zsh
@@ -3,7 +3,6 @@ export PATH=$(chezmoi source-path)/_/bin:$PATH
 
 # asdf
 source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
-. /opt/homebrew/opt/asdf/libexec/asdf.sh
 
 # rmをゴミ箱に
 alias rm='trash'
