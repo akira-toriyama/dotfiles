58ac04f3560c0012fa2c7ac7153f2144bd383da2
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2024-08-21 15:42:18 +0900

    :adhesive_bandage:`x.x.o`: Fix init sh (#87)
    * :adhesive_bandage:`x.x.o`: Fix init sh
    
    * :pencil2:`x.x.o`: Fix CI error
    
    * :pencil2:`x.x.o`: Fix
    
    * :pencil2:`x.x.o`: Fix
    
    * :pencil2:`x.x.o`: Fix
    
    * :pencil2:`x.x.o`: Fix

--- _/zsh/my.zsh
+++ _/zsh/my.zsh
@@ -3,8 +3,6 @@ export PATH=$(chezmoi source-path)/_/bin:$PATH
 
 # asdf
 . $(brew --prefix asdf)/libexec/asdf.sh
-# asdf-direnv
-source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
 
 # rmをゴミ箱に
 alias rm='trash'
